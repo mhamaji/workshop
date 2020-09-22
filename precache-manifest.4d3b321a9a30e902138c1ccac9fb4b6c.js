4226deb05c3fe38634a22456b35e2208df8a0e5d
Mauricio Homma <mhhomma@gmail.com>
2020-09-22 16:32:15 -0300

    Updates

---
+++ precache-manifest.4d3b321a9a30e902138c1ccac9fb4b6c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3942169540a12aaa84768754a63e6188",
+    "url": "/workshop/index.html"
+  },
+  {
+    "revision": "ee0d0f36d43b279d61e2",
+    "url": "/workshop/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a1bc9d9a23471ee2961f",
+    "url": "/workshop/static/js/2.653b8a97.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/workshop/static/js/2.653b8a97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee0d0f36d43b279d61e2",
+    "url": "/workshop/static/js/main.2d02eaf8.chunk.js"
+  },
+  {
+    "revision": "d1ad9c7924c7ed342868",
+    "url": "/workshop/static/js/runtime-main.427eb774.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/workshop/static/media/logo.5d5d9eef.svg"
+  }
+]);
